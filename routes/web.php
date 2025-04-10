ae32d572daf8e7d9dac92445ad168983dd18382f
CharlyXV <167381351+CharlyXV@users.noreply.github.com>
2025-04-10 15:32:40 -0700

    Cambios Red

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,28 @@ use App\Http\Controllers\PrestamosController;
 use App\Http\Controllers\LanguageController;
 use App\Http\Controllers\ReportPayController;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 $routes = function () {
     Route::get('/', function () {
         return view('welcome');
     });
 
-// Ruta corregida (agrega el parámetro {prestamo} a la URL)
-Route::get('/loan/{prestamo}', [ReportPayController::class, 'generateReport'])->name('pay.report');
+    /*Route::get('/admin/createAdminUser', function () {
+        $user = new User();
+        $user->name = 'Administrator';
+        $user->username = 'Administrator';
+        $user->password = Hash::make('Password1');
+        $user->email = 'desarrolladorob@corporacionob.com';
+        $user->save();
+        return response()->json($user, 200);
+    });*/
+
+    // Ruta corregida (agrega el parámetro {prestamo} a la URL)
+    Route::get('/loan/{prestamo}', [ReportPayController::class, 'generateReport'])->name('pay.report');
 };
+
 Route::middleware('custom.throttle')->group(function () {
     // Rutas que requieren limitación de tasa
 });
